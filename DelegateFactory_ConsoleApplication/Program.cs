9feb08993d0beec193e6af9423eda691285b2597
programmiertier <zimmermannveronique@protonmail.com>
2017-11-13 14:38:41 +0100

    Katzen in Listen

--- DelegateFactory_ConsoleApplication/Program.cs
+++ DelegateFactory_ConsoleApplication/Program.cs
@@ -3,13 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace DelegateFactory_ConsoleApplication
 {
     class Program
     {
+        public delegate void Del_Katz(string kaetzchen);
+
         static void Main(string[] args)
         {
+            /*Del_Katz miez;
+
+            miez = new Del_Katz(StepsToBuild.rohKatze_bereitstellen);
+            miez += new Del_Katz(StepsToBuild.pfoten_bereitstellen);
+            miez += new Del_Katz(StepsToBuild.pfoten_einsetzen);
+            miez += new Del_Katz(StepsToBuild.schnurrhaare_montieren);
+            miez += new Del_Katz(StepsToBuild.pfoten_schnurrhaare_verbinden);
+
+            /* miez("Schnurri");
+            ReadLine();
+
+            miez -= new Del_Katz(StepsToBuild.pfoten_schnurrhaare_verbinden);
+            miez("Mauzi");
+            ReadLine();*/
+
+            Del_Katz Katze_bauen = new Del_Katz((string miez) => { });
+
+            List<Del_Katz> katzenliste = new List<Del_Katz>() {
+                                                    StepsToBuild.rohKatze_bereitstellen,
+                                                    StepsToBuild.pfoten_bereitstellen,
+                                                    StepsToBuild.pfoten_einsetzen,
+                                                    StepsToBuild.schnurrhaare_montieren,
+                                                    StepsToBuild.pfoten_schnurrhaare_verbinden  };
+            foreach(var sprung in katzenliste)
+            {
+                Katze_bauen += sprung;
+            }
+            Katze_bauen("mauzi");
+            ReadLine();
+            Katze_bauen("miez");
+            ReadLine();
+            Katze_bauen("Sally");
+            ReadLine();
+            Katze_bauen("Aurora");
+
         }
     }
 }
---
+++ DelegateFactory_ConsoleApplication/StepsToBuild.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DelegateFactory_ConsoleApplication
+{
+    class StepsToBuild
+    {
+        public static void rohKatze_bereitstellen(string katzi)
+        {
+            WriteLine("Die Rohkatze_bereitstellen ({0})", katzi);
+        }
+        public static void pfoten_bereitstellen(string katzi)
+        {
+            WriteLine("Die Pfoten bereitstellen ({0})", katzi);
+        }
+        public static void schnurrhaare_montieren(string katzi)
+        {
+            WriteLine("Die Schnurrhaare montieren ({0})", katzi);
+        }
+        public static void pfoten_einsetzen(string katzi)
+        {
+            WriteLine("Die Pfoten einsetzen ({0})", katzi);
+        }
+        public static void pfoten_schnurrhaare_verbinden(string katzi)
+        {
+            WriteLine("Die Pfoten und Schnurrhaare verbinden ({0})", katzi);
+        }
+    }
+}
